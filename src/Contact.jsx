6620109f4b4b11a79a78d649e0f5ef4b7ca9a441
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2021-01-02 02:10:46 +0530

    minor change to form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,7 +7,7 @@ class Contact extends Component {
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
-              <h3 className="text-lg font-medium leading-6 text-gray-900">
+              <h3 className="text-3xl font-custom leading-6 text-gray-900">
                 Contact Me
               </h3>
               <p className="mt-1 text-sm text-gray-600">

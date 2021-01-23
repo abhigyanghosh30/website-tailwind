c93a2c1a56931f2b96d9c11b1396db20bb3b8d90
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2021-01-24 00:03:22 +0530

    trying to add fullpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab, faInstagram,faFacebook, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+  fab,
+  faInstagram,
+  faFacebook,
+  faLinkedin,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
 
 import './App.css';
 import Home from './Home';
 import About from './About';
-import Navbar from './Navbar';
-import Footer from './Footer';
 
-library.add(fab, faInstagram,faFacebook,faGithub,faLinkedin);
+library.add(fab, faInstagram, faFacebook, faGithub, faLinkedin);
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Switch>
         <Route exact path="/" component={Home}></Route>
       </Switch>
       <Switch>
         <Route exact path="/about" component={About}></Route>
       </Switch>
-      <Footer />
     </BrowserRouter>
   );
 }

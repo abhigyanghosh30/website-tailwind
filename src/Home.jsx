c78b0863c1168180755cae8812ff10cf8d9fff39
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2021-01-31 03:36:19 +0530

    glass effect works only in chrome

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import { Component } from 'react';
 import Contact from './Contact';
 import Hero from './Hero';
 import ReactFullpage from '@fullpage/react-fullpage';
-
+import './Home.css';
 const Fullpage = () => (
   <ReactFullpage
     //fullpage options
@@ -10,11 +10,17 @@ const Fullpage = () => (
     render={({ state, fullpageApi }) => {
       return (
         <ReactFullpage.Wrapper>
-          <div className="section">
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Click me to move down
-            </button>
+          <div className="section bg-gradient-to-tr from-glass-100 to-glass-300">
+            <div className="flex items-center justify-center">
+              <div className="glass z-20 min-h-3/4 w-8/12 bg-gradient-to-br from-glass-white-700 to-glass-white-300 rounded-3xl">
+                <p>Section 1 (welcome to fullpage.js)</p>
+                <button onClick={() => fullpageApi.moveSectionDown()}>
+                  Click me to move down
+                </button>
+              </div>
+              <div className="circle2 z-10 absolute top-10 right-40 h-80 w-80 bg-gradient-to-br from-glass-white-700 to-glass-white-300 rounded-full"></div>
+              <div className="circle1 z-10 absolute bottom-10 left-40 h-80 w-80 bg-gradient-to-br from-glass-white-700 to-glass-white-300 rounded-full"></div>
+            </div>
           </div>
           <div className="section">
             <p>Section 2</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,23 @@ module.exports = {
   purge: [],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
+    minHeight: {
+      0: '0',
+      '1/4': '25vh',
+      '1/2': '50vh',
+      '3/4': '75vh',
+      full: '100%',
+      screen: '100vh',
+    },
     colors: {
       black: colors.black,
       white: colors.white,
       gray: colors.gray,
+      transparent: 'transparent',
+      current: 'currentColor',
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
       silver: {
         100: '#fafafa',
         300: '#d4d4d4',
@@ -23,6 +36,17 @@ module.exports = {
         900: '#111628',
       },
       'regal-gold': '#bba559',
+      glass: {
+        100: '#65dfc9',
+        300: '#6cdbeb',
+      },
+      'glass-white': {
+        100: 'rgba(255,255,255,0.1)',
+        300: 'rgba(255,255,255,0.3)',
+        500: 'rgba(255,255,255,0.5)',
+        700: 'rgba(255,255,255,0.7)',
+        900: 'rgba(255,255,255,0.9)',
+      },
     },
     fontFamily: {
       custom: ['URW Gothic L', 'sans-serif'],

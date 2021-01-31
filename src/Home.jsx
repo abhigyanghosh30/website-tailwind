8256a2f13be42ceebe3f88b145e1eb7ff64a0891
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2021-01-31 22:23:12 +0530

    trying to use blur to get glass effect

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ const Fullpage = () => (
     render={({ state, fullpageApi }) => {
       return (
         <ReactFullpage.Wrapper>
-          <div className="section bg-gradient-to-tr from-glass-100 to-glass-300">
+          <div className="section">
             <div className="flex items-center justify-center">
               <div className="glass z-20 min-h-3/4 w-8/12 bg-gradient-to-br from-glass-white-700 to-glass-white-300 rounded-3xl">
                 <p>Section 1 (welcome to fullpage.js)</p>
@@ -18,8 +18,8 @@ const Fullpage = () => (
                   Click me to move down
                 </button>
               </div>
-              <div className="circle2 z-10 absolute top-10 right-40 h-80 w-80 bg-gradient-to-br from-glass-white-700 to-glass-white-300 rounded-full"></div>
-              <div className="circle1 z-10 absolute bottom-10 left-40 h-80 w-80 bg-gradient-to-br from-glass-white-700 to-glass-white-300 rounded-full"></div>
+              {/* <div className="circle2 z-10 absolute top-10 right-40 h-80 w-80 bg-gradient-to-br from-glass-white-700 to-glass-white-300 rounded-full"></div>
+              <div className="circle1 z-10 absolute bottom-10 left-40 h-80 w-80 bg-gradient-to-br from-glass-white-700 to-glass-white-300 rounded-full"></div> */}
             </div>
           </div>
           <div className="section">
